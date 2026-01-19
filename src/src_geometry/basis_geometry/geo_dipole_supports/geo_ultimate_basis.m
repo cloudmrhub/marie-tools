57fd62e1f212b157f5b2740ae10969a00b40adfd
Ilias I. Giannakopoulos <iliasgiannako@gmail.com>
2026-01-19 17:25:11 -0500

    Refactor geo_ultimate_basis.m for proper basis centering

--- src/src_geometry/basis_geometry/geo_dipole_supports/geo_ultimate_basis.m
+++ src/src_geometry/basis_geometry/geo_dipole_supports/geo_ultimate_basis.m
@@ -5,55 +5,47 @@ function [MREDM] = geo_ultimate_basis(MREDM)
     n2  = MREDM.dimensions.n2;
     n3  = MREDM.dimensions.n3;
     res = MREDM.dimensions.res;
-    dis = inp.Basis_distance;
-    thi = inp.Basis_thickness;
+
+    dis = inp.Basis_distance;   
+    thi = inp.Basis_thickness;  
 
     dis = floor(dis/res);
+
+    pad = dis + thi;
+
     mask = MREDM.dimensions.mask;
-    [size_x, size_y, size_z] = size(mask);
-    new_size_x = size_x + 2*dis + 2*thi;
-    new_size_y = size_y + 2*dis + 2*thi;
-    new_size_z = size_z + 2*dis + 2*thi;
-    new_mask = zeros(new_size_x, new_size_y, new_size_z);
-    center_x = ceil(new_size_x/2);
-    center_y = ceil(new_size_y/2);
-    center_z = ceil(new_size_z/2);
-    start_x = center_x - floor(size_x/2)+1;
-    end_x = center_x + ceil(size_x/2);
-    start_y = center_y - floor(size_y/2)+1;
-    end_y = center_y + ceil(size_y/2);
-    start_z = center_z - floor(size_z/2);
-    end_z = center_z + ceil(size_z/2)-1;
-    new_mask(start_x:end_x, start_y:end_y, start_z:end_z) = mask;
-    disA = imdilate(new_mask, strel('sphere', dis));
-    disThiA = imdilate(new_mask, strel('sphere', dis+thi));
+
+    r = MREDM.dimensions.r; 
+
+    x0 = squeeze(r(:,1,1,1));   
+    y0 = squeeze(r(1,:,1,2));  
+    z0 = squeeze(r(1,1,:,3));   
+
+    y0 = y0(:).';
+    z0 = z0(:).';
+
+    x = (x0(1) - pad*res) : res : (x0(end) + pad*res);
+    y = (y0(1) - pad*res) : res : (y0(end) + pad*res);
+    z = (z0(1) - pad*res) : res : (z0(end) + pad*res);
+
+    new_size_x = numel(x);
+    new_size_y = numel(y);
+    new_size_z = numel(z);
+
+    new_mask = zeros(new_size_x, new_size_y, new_size_z, 'like', mask);
+
+    new_mask( (pad+1):(pad+n1), (pad+1):(pad+n2), (pad+1):(pad+n3) ) = mask;
+
+    disA    = imdilate(new_mask, strel('sphere', dis));
+    disThiA = imdilate(new_mask, strel('sphere', dis + thi));
+
     maskB = imsubtract(disThiA, disA);
     maskB = bwareaopen(maskB, 1);
-    x = [-new_size_x/2*res new_size_x/2*res];
-    y = [-new_size_y/2*res new_size_y/2*res];
-    z = [-new_size_z/2*res new_size_z/2*res];
-    nx = ceil((max(x)-min(x))/2/res);
-    ny = ceil((max(y)-min(y))/2/res);
-    nz = ceil((max(z)-min(z))/2/res);
-    Cx = (max(x)+min(x))/2;
-    Cy = (max(y)+min(y))/2;
-    Cz = (max(z)+min(z))/2;
-    a = 1; b = 1; c = 1;
-    if new_size_x ~= nx*2
-        a = 2;
-    end
-    if new_size_y ~= ny*2
-        b = 2;
-    end
-    if new_size_z ~= nz*2
-        c = 2;
-    end
-    x = Cx-nx*res:res:Cx+(nx-a)*res;
-    y = Cy-ny*res:res:Cy+(ny-b)*res;
-    z = Cz-nz*res:res:Cz+(nz-c)*res;
-    r_basis = grid3d(x,y,z);
-    basis_idxS = find(maskB>0);
-    
+
+    r_basis = grid3d(x, y, z);
+
+    basis_idxS = find(maskB > 0);
+
     [nb1,nb2,nb3,~]                              = size(r_basis);
     mask_basis                                   = zeros(nb1,nb2,nb3);
     s1                                           = ceil((nb1-n1)/2);
@@ -62,16 +54,20 @@ function [MREDM] = geo_ultimate_basis(MREDM)
     nbv                                          = nb1*nb2*nb3;
     nI                                           = length(basis_idxS);
     basis_idx_solution                           = zeros(MREDM.dimensions.ql*nI,1);
+
     mask_basis(s1+1:n1+s1,s2+1:n2+s2,s3+1:n3+s3) = MREDM.dimensions.mask;
+
     basis_idxS_solve                             = find(abs(mask_basis)>0);
     nbI                                          = length(basis_idxS_solve);
     basis_idx_solution_solve                     = zeros(MREDM.dimensions.ql*nbI,1);
+
     for ii = 1:MREDM.dimensions.ql
         basis_idx_solution( (ii-1)*nI+1:ii*nI ) = (ii-1)*nbv + basis_idxS;
     end
     for ii = 1:MREDM.dimensions.ql
         basis_idx_solution_solve( (ii-1)*nbI+1:ii*nbI ) = (ii-1)*nbv + basis_idxS_solve;
     end
+
     MREDM.dimensions.mask_basis               = mask_basis;
     MREDM.dimensions.basis_idxS_solve         = basis_idxS_solve;
     MREDM.dimensions.basis_idx_solution_solve = basis_idx_solution_solve;
