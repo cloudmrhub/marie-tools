e9ba4f2bd2c6e395dcdc5e42172c3f9c1b806ed3
Ilias I. Giannakopoulos <iliasgiannako@gmail.com>
2026-02-18 11:03:56 -0500

    Add files via upload

--- src/src_solver/src_preconditioners/prec_wsvie.m
+++ src/src_solver/src_preconditioners/prec_wsvie.m
@@ -1,57 +1,65 @@
 function [MREDM] = prec_wsvie(MREDM)
+    
+    if MREDM.inputs.FMM_flag == 0
 
-    % Wire-Coil-Shield
-    if ~isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
-        Zss  = MREDM.SIE.Z_shield;
-        Zsw  = MREDM.operators.Zsw;      
-        ZscU = MREDM.operators.Zsc.U;    
-        ZscV = MREDM.operators.Zsc.V';
-        Zsc  = ZscU*ZscV;
-        ZcwU = MREDM.operators.Zcw.U;  
-        ZcwV = MREDM.operators.Zcw.V';
-        Zcw  = ZcwU*ZcwV;
-        Zww  = MREDM.WIE.Z;
-        Zcc  = MREDM.SIE.Z;
-        A    = [Zss   Zsw   Zsc;   
-                Zsw.' Zww   Zcw;  
-                Zsc.' Zcw.' Zcc];    
-    % Wire-Coil   
-    elseif ~isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && isempty(MREDM.SIE.shield)
-        ZcwU = MREDM.operators.Zcw.U;  
-        ZcwV = MREDM.operators.Zcw.V';
-        Zcw  = ZcwU*ZcwV;
-        Zww  = MREDM.WIE.Z;
-        Zcc  = MREDM.SIE.Z;
-        A    = [Zww   Zcw;  
-                Zcw.' Zcc];   
-    % Coil-Shield
-    elseif isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
-        Zss  = MREDM.SIE.Z_shield;
-        ZscU = MREDM.operators.Zsc.U;    
-        ZscV = MREDM.operators.Zsc.V';
-        Zsc  = ZscU*ZscV;
-        Zcc  = MREDM.SIE.Z;
-        A    = [Zss   Zsc;   
-                Zsc.' Zcc]; 
-    % Wire-Shield
-    elseif ~isempty(MREDM.WIE.coil) && isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
-        Zss  = MREDM.SIE.Z_shield;
-        Zsw  = MREDM.operators.Zsw;      
-        Zww  = MREDM.WIE.Z;
-        A    = [Zss   Zsw;   
-                Zsw.' Zww];  
-    % Coil
-    elseif isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && isempty(MREDM.SIE.shield)
-        A  = MREDM.SIE.Z;
-    % Wire
-    elseif ~isempty(MREDM.WIE.coil) && isempty(MREDM.SIE.coil) && isempty(MREDM.SIE.shield)
-        A  = MREDM.WIE.Z;
-    % Shield
-    elseif isempty(MREDM.WIE.coil) && isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
-        A  = MREDM.SIE.Z_shield;
+        % Wire-Coil-Shield
+        if ~isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
+            Zss  = MREDM.SIE.Z_shield;
+            Zsw  = MREDM.operators.Zsw;      
+            ZscU = MREDM.operators.Zsc.U;    
+            ZscV = MREDM.operators.Zsc.V';
+            Zsc  = ZscU*ZscV;
+            ZcwU = MREDM.operators.Zcw.U;  
+            ZcwV = MREDM.operators.Zcw.V';
+            Zcw  = ZcwU*ZcwV;
+            Zww  = MREDM.WIE.Z;
+            Zcc  = MREDM.SIE.Z;
+            A    = [Zss   Zsw   Zsc;   
+                    Zsw.' Zww   Zcw;  
+                    Zsc.' Zcw.' Zcc];    
+        % Wire-Coil   
+        elseif ~isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && isempty(MREDM.SIE.shield)
+            ZcwU = MREDM.operators.Zcw.U;  
+            ZcwV = MREDM.operators.Zcw.V';
+            Zcw  = ZcwU*ZcwV;
+            Zww  = MREDM.WIE.Z;
+            Zcc  = MREDM.SIE.Z;
+            A    = [Zww   Zcw;  
+                    Zcw.' Zcc];   
+        % Coil-Shield
+        elseif isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
+            Zss  = MREDM.SIE.Z_shield;
+            ZscU = MREDM.operators.Zsc.U;    
+            ZscV = MREDM.operators.Zsc.V';
+            Zsc  = ZscU*ZscV;
+            Zcc  = MREDM.SIE.Z;
+            A    = [Zss   Zsc;   
+                    Zsc.' Zcc]; 
+        % Wire-Shield
+        elseif ~isempty(MREDM.WIE.coil) && isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
+            Zss  = MREDM.SIE.Z_shield;
+            Zsw  = MREDM.operators.Zsw;      
+            Zww  = MREDM.WIE.Z;
+            A    = [Zss   Zsw;   
+                    Zsw.' Zww];  
+        % Coil
+        elseif isempty(MREDM.WIE.coil) && ~isempty(MREDM.SIE.coil) && isempty(MREDM.SIE.shield)
+            A  = MREDM.SIE.Z;
+        % Wire
+        elseif ~isempty(MREDM.WIE.coil) && isempty(MREDM.SIE.coil) && isempty(MREDM.SIE.shield)
+            A  = MREDM.WIE.Z;
+        % Shield
+        elseif isempty(MREDM.WIE.coil) && isempty(MREDM.SIE.coil) && ~isempty(MREDM.SIE.shield)
+            A  = MREDM.SIE.Z_shield;
+        end
+
+        MREDM = prec_LU(MREDM,A);
+
+    elseif MREDM.inputs.FMM_flag == 1
+        
+        MREDM = prec_Block_Jacobi_LU(MREDM,1);
+
     end
-
-    MREDM = prec_LU(MREDM,A);
 
     MREDM = prec_vie(MREDM);
     
